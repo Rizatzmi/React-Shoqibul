52b631b1b047fa80473be8b7b579785d208dbdf7
Rizatzmi <Rizatzmi19@gmail.com>
2024-03-03 08:19:18 +0700

    Minor Update

--- src/Data/Project.js
+++ src/Data/Project.js
@@ -7,7 +7,7 @@ const projects = [
       "Convert a PDF file that has several fields and has data that has been filled in to an Excel file that is exactly the same as in the PDF file. This is just a preview. If you'd like to see the complete version, please click the 'Download' button below.",
     imageUrl: "/Images/PDMI WEST.png",
     file: "PDMI WEST 2021.xlsx",
-    fileUrl: "Docs/PDMI WEST 2021.xlsx",
+    fileUrl: "/Docs/PDMI WEST 2021.xlsx",
   },
   {
     id: 2,
@@ -16,7 +16,7 @@ const projects = [
     description: `Collecting data on 5 star hotels in Paris from the <a href="https://www.tripadvisor.com/Hotels-g187147-zfa9176-zfc5-zfd21371-a_trating.40-a_ufe.true-Paris_Ile_de_France-Hotels.html" style="color: blue; text-decoration: underline;">tripadivsor.com</a> website with the following filters: Free Wifi and 4+ Rating at the end of 2023. This is just a preview. If you'd like to see the complete version, please click the 'Download' button below.`,
     imageUrl: "/Images/Paris Hotel.jpeg",
     file: "Paris 5 Star Hotels.xlsx",
-    fileUrl: "Docs/Paris 5 Star Hotels.xlsx",
+    fileUrl: "/Docs/Paris 5 Star Hotels.xlsx",
   },
   {
     id: 3,
@@ -25,7 +25,7 @@ const projects = [
     description: `Collecting data from several foundation companies to fill in the required fields such as <b>LinkedIn Profile</b> and <b>Email Address</b>. This is just a preview. If you'd like to see the complete version, please click the 'Download' button below.`,
     imageUrl: "/Images/Foundation.jfif",
     file: "Company Records.xlsx",
-    fileUrl: "Docs/Company Records.xlsx",
+    fileUrl: "/Docs/Company Records.xlsx",
   },
   {
     id: 4,
@@ -34,7 +34,7 @@ const projects = [
     description: `Visualizing dataset of Smoking Mortality from <a href="https://www.kaggle.com/datasets/willianoliveiragibin/smoking-crisis" style="color: blue; text-decoration: underline;">kaggle.com</a> to Excel and Website as below. This is just a preview. If you'd like to see the complete version, please click the 'Download' button below.`,
     imageUrl: "/Images/Smoking.jpeg",
     file: "Death Rate Smoking.xlsx",
-    fileUrl: "Docs/Death Rate Smoking.xlsx",
+    fileUrl: "/Docs/Death Rate Smoking.xlsx",
   },
 ];
 
